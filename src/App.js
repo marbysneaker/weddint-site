8b6d0b225f2df1dcf2951afc982972cc7f32eac4
marby <marbysneaker@gmail.com>
2020-03-26 16:41:32 -0700

    added button

--- src/App.js
+++ src/App.js
@@ -30,12 +30,19 @@ function App() {
           <div className='main-title'>
           Wedding Florist  
           <br/>
+          <span>RICHARD CLEMENTE</span>
+          <br/>
           <span><a href="https://www.facebook.com/" ><FacebookIcon /></a>
           <a href="https://www.twitter.com"><TwitterIcon/></a><a href="https://www.instagram"><InstagramIcon/></a></span>
           </div>       
           <div className='main-des'>
           Richard Clemente <br/>
           gay courier from falck
+          A 34-year-old Italian nurse treating COVID-19 patients in the hard-struck European country has taken her own life for fear of having spread the illness, according to a nursing organization there.
+          Daniela Trezzi worked in the intensive care unit of San Gerardo of Monza Hospital, according to a translated statement from the National Federation of Nurses of Italy, in a region described as “one of the major Italian fronts of the pandemic.”
+
+          Trezzi and her colleagues treating quarantined patients showing coronavirus symptoms felt “heavy stress for fear of having infected others,” according to the federation, which noted the “pain and dismay” of its members “at the news of a young colleague who has not made it.”
+          <br/><button>Contact</button>
           </div>
           </div>
         
