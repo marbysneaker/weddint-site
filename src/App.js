ff86337d112a9a4c48bf2aa12312d7b93eb8ccc4
marby <marbysneaker@gmail.com>
2020-03-26 22:44:28 -0700

    added content

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Wedding from './wedding.png'
+import Wedding2 from './wedding2.jpg'
 import FacebookIcon from '@material-ui/icons/Facebook'
 import TwitterIcon from '@material-ui/icons/Twitter'
 import InstagramIcon from '@material-ui/icons/Instagram';
@@ -23,9 +24,7 @@ function App() {
           <div className="main-photo">
             
           </div>
-          <div className="main-example">
           
-          </div>
           <div className='main-content'>
           <div className='main-title'>
           Wedding Florist  
@@ -45,7 +44,29 @@ function App() {
           <br/><button>Contact</button>
           </div>
           </div>
-        
+          <div className="main-example">
+            <div className='example-title'>
+            " We are located in the Bay Area "</div>
+            <div className='example-1'>
+            <img src={Wedding2}/>
+            </div>
+            <div className='example-1-des'>
+            Philippines Armed Forces Chief Felimon Santos Jr. has tested positive for the coronavirus, according to CNN Philippines.
+
+            Santos has been on home quarantine since March 24 after coming into contact with a senior officer who had tested positive for the virus. Santos received his results Thursday night and informed the Secretary of Defense Delfin Lorenzana, CNN Philippines reported.
+
+            There are 707 confirmed cases of the coronavirus in the Philippines, and 45 deaths, according to Johns Hopkins.
+            </div>
+            <div className='example-2'>
+            </div>
+            <div className='example-2-des'>
+            There are now 25 sailors who have tested positive for the coronavirus aboard the aircraft carrier USS Theodore Roosevelt, just two days after the Pentagon announced that three sailors aboard the ship had tested positive for the virus, a Navy official has confirmed to CNN.
+
+            The Navy says they expect there to be additional positive tests among the crew, with one official telling CNN there could possibly be "dozens" of new cases that emerge. A second official said that were there to be a large number of additional cases, the Defense Department would be unlikely to publicly specify how many of the Navy's overall cases are amongst members of the crew of the Roosevelt, due to concerns that adversaries such as China or North Korea could see the ship as vulnerable.
+            </div>
+
+          
+          </div>
 
         
         </div>
