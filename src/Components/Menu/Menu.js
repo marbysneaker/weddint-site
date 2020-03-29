7bf2b2cc5ab2ad5ce9c2f5541ec60f2a87e3d3df
marby <marbysneaker@gmail.com>
2020-03-29 14:07:25 -0700

    started menu

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import './Menu.css'
+import Wedding from '../../wedding.png'
+import Wedding2 from '../../wedding2.jpg'
+import Wedding3 from '../../wedding3.jpg'
+import Wedding4 from '../../wedding4.jpg'
+import Wedding5 from '../../wedding5.jpg'
 
 class Menu extends Component {
     render(){
@@ -7,7 +12,37 @@ class Menu extends Component {
 
             <div className='menu'>
                 <div className='menu-title'>
-                MENU</div>
+                " Menu "</div>
+                <div className ='menu-list'>
+                <ul>
+                    <li>Flower2</li>
+                    <br/>
+                    <li>Flower3</li>
+                    <br/>
+                    <li>Flower4</li>
+                    <br/>
+                    <li>Flower5</li>
+                    <br/>
+                    <li>Flower6</li>
+                    <br/>
+                    <li>Flower7</li>
+                    <br/>
+                    <li>Flower8</li>
+                    <br/>
+                    <li>Flower9</li>
+                    <br/>
+                </ul></div>
+                <div className='menu-images'>
+                    <div className='img1'>
+                        <img src={Wedding3}/>
+                    </div>
+                    <div className='img2'>
+                        <img src={Wedding4}/>
+                    </div>
+                    <div className='img3'>
+                        <img src={Wedding5}/>
+                    </div>
+                </div>
             </div>
         )
 
