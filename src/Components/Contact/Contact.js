4c384882cf3bf410226f47a8b34c97695064227d
marby <marbysneaker@gmail.com>
2020-03-28 19:15:53 -0700

    working on contact

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import './Contact.css'
+import Wedding7 from './wedding7.jpg';
+import Wedding8 from './wedding8.jpg';
+import Wedding9 from './wedding9.jpg';
 
 
 class Contact extends Component {
@@ -8,10 +11,15 @@ class Contact extends Component {
             <div className='contact'>
                 <div className='contact-title'>
                 Contact Us</div>
+                <div className='contact-image'>
+                    <img src={Wedding7} />
+                    <img src={Wedding8} />
+                    <img src={Wedding9} />
+                </div>
             </div>
 
 
         )
     }
 }
-export default Contact
+export default Contact;
